313a6940e2eb9d190c6cc071a4fddf44c5547ced
pouya ak <pouyaakn4@gmail.com>
2022-08-10 17:00:42 +0430

    first commit

---
+++ hello.sh
@@ -0,0 +1,16 @@
+#!C:\\Progra~1\\Git\bin\\bash.exe
+
+# echo "Hello bash scripting" 
+# echo "Hello bash scripting" > helle.txt      Write to File
+# cat > Hello.txt                              Write to File from Terminal by pres ctl+D
+# cat >> Hello.txt                             Append to File from Terminal by pres ctl+D
+
+: '
+this is multi line comment
+this is multi line comment
+this is multi line comment
+this is multi line comment
+this is multi line comment
+this is multi line comment
+this is multi line comment
+this is multi line comment'
